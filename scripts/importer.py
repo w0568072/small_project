df43f8199c696b961af4ac99df54044eb14804ac
Katelyn Small <katelyn.small@selu.edu>
2018-10-11 19:20:18 +0000

    fixed it

--- scripts/importer.py
+++ scripts/importer.py
@@ -2,6 +2,7 @@ import dendropy
 
 amphib = dendropy.DnaCharacterMatrix.get(
     path="../data/plethodon.phy",
+    schema="phylip"
 )
 
 amphib.write_to_path("../data/plethodon.fa", schema="fasta")
